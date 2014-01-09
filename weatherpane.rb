4ace17788f91a39996db67a9a0a815376c05f50b
Tom Hanlon <tomhanlon@gmail.com>
2014-01-08 21:11:24 -0600

    Attempt to make erb version work before converting haml

--- weatherpane.rb
+++ weatherpane.rb
@@ -3,13 +3,13 @@ require 'sinatra'
 require 'haml'
 require 'unirest'
 
-
 get '/' do  
   haml :index 
 end
 
 get '/location/:latitude/:longitude' do
-
+  erb :weather
+  
   controller_lat = params[:latitude]
   controller_long = params[:longitude]
 
@@ -23,12 +23,13 @@ get '/location/:latitude/:longitude' do
     end
   response = Unirest.get("https://api.forecast.io/forecast/#{ENV['FORECAST_KEY']}/#{new1},#{new2}")
   @current_condition = response.body['currently']['icon']
-  @current_temp = response.body['currently']['temperature']
+  @current_temp = response.body['currently']['temperature'].ceil
 
   place = Unirest.get("http://maps.googleapis.com/maps/api/geocode/json?latlng=#{new1},#{new2}&sensor=true")
   
   @current_city = place.body['results'][0]['address_components'][3]['long_name']
   @current_state = place.body['results'][0]['address_components'][5]['short_name']
 
-  @icon = "/images/#{ @current_condition }.jpg"         
+  @icon = "/images/#{ @current_condition }.jpg"
+         
 end
