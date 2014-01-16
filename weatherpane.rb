85e97685cef58c3dae8972d653b62232858b5971
Tom Hanlon <tomhanlon@gmail.com>
2014-01-16 17:10:51 -0600

    Fix link to stylesheet and search through JSON data for city and state by keys instead of index position.

--- weatherpane.rb
+++ weatherpane.rb
@@ -8,7 +8,7 @@ get '/' do
 end
 
 get '/location/:latitude/:longitude' do
-  erb :weather
+  
   
   controller_lat = params[:latitude]
   controller_long = params[:longitude]
@@ -21,15 +21,29 @@ get '/location/:latitude/:longitude' do
     elsif new1[0] =='n'
       new1 = new1.gsub('n','-')
     end
+
   response = Unirest.get("https://api.forecast.io/forecast/#{ENV['FORECAST_KEY']}/#{new1},#{new2}")
+
   @current_condition = response.body['currently']['icon']
   @current_temp = response.body['currently']['temperature'].ceil
 
   place = Unirest.get("http://maps.googleapis.com/maps/api/geocode/json?latlng=#{new1},#{new2}&sensor=true")
   
-  @current_city = place.body['results'][0]['address_components'][3]['long_name']
-  @current_state = place.body['results'][0]['address_components'][5]['short_name']
+  locations = place.body['results'][0]["address_components"]
 
-  @icon = "/images/#{ @current_condition }.jpg"
+  @current_city = nil
+  @@current_state = nil
+
+  locations.each do |location|
+    if location["types"] == ["locality", "political"]
+      @current_city = location["long_name"]
+    end
+
+    if location["types"] == ["administrative_area_level_1", "political"]
+      @current_state = location["long_name"]
+    end
+  end
+
+  erb :weather
          
 end
